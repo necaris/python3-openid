fa044845cbf7a01df6699053d530ca235a00e377
Rami Chowdhury <rami.chowdhury@gmail.com>
2013-04-15 01:48:55 +0100

    Indentation & cleanup

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import sys
 import os
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
 if 'sdist' in sys.argv:
     os.system('./admin/makedoc')
@@ -22,30 +19,33 @@ single sign-on for your web site?  Use the openid.consumer package.  Want to
 run your own OpenID server? Check out openid.server.  Includes example code
 and support for a variety of storage back-ends.''',
     url='http://github.com/necaris/python3-openid',
-    packages=['openid',
-              'openid.consumer',
-              'openid.server',
-              'openid.store',
-              'openid.yadis',
-              'openid.extensions',
-              'openid.extensions.draft',
-              ],
-    # license specified by classifier.
-    # license=getLicense(),
+    packages=[
+        'openid',
+        'openid.consumer',
+        'openid.server',
+        'openid.store',
+        'openid.yadis',
+        'openid.extensions',
+        'openid.extensions.draft',
+    ],
+    # license specified by classifier
     author='Rami Chowdhury',
     author_email='rami.chowdhury@gmail.com',
-    download_url='http://github.com/necaris/python3-openid/tarball/v%s' % (version,),
+    download_url=('http://github.com/necaris/python3-openid/tarball'
+                  '/v{}'.format(version)),
     classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "Environment :: Web Environment",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: Apache Software License",
-    "Operating System :: POSIX",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Topic :: Internet :: WWW/HTTP",
-    "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Topic :: System :: Systems Administration :: Authentication/Directory",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP",
+        ("Topic :: Internet :: WWW/HTTP :: Dynamic Content :: "
+         "CGI Tools/Libraries"),
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ("Topic :: System :: Systems Administration :: "
+         "Authentication/Directory"),
     ]
-    )
+)
